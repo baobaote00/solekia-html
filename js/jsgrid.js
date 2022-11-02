0c8743e8927f339c58acf6bb0ec0cafa349d4329
お金 <57231517+baobaote00@users.noreply.github.com>
2022-11-02 14:11:46 +0700

    Update jsgrid.js

--- js/jsgrid.js
+++ js/jsgrid.js
@@ -1,5 +1,5 @@
 var clients = [
-  { "Name": "nguyen le trong tien", "Sex": "Male", "Age": 21, "Hobby": "listen music", "Image": "./images/tien.jpg" },
+  { "Name": "nguyen le trong tien", "Sex": "Male", "Age": 21, "Hobby": "listen music", "Image": "./images/tien.jpg","Host": "https://baobaote00.github.io/solekia-html/", },
 ];
 
 $("#jsGrid").jsGrid({
@@ -16,8 +16,9 @@ $("#jsGrid").jsGrid({
     { name: "Name", type: "text", width: 150, validate: "required",itemTemplate: function (value, item) { return `<span style"text-transform: capitalize;">${value}</span>` }},
     { name: "Sex", type: "text", width: 50 },
     { name: "Age", type: "number", width: 50 },
-    { name: "Hobby", type: "text", width: 200 },
-    { name: "Image", type: "text", width: 200, itemTemplate: function (value, item) { return `<img class="img-fluid w-25" src='${value}'/>` } },
+    { name: "Hobby", type: "text", width: 150 },
+    { name: "Image", type: "text", width: 100, itemTemplate: function (value, item) { return `<img class="img-fluid w-25" src='${value}'/>` } },
+    { name: "Host", type: "text", width: 200, itemTemplate: function (value, item) { return `<a href=${value}>${value}</a>` } },
     { type: "control" }
   ]
 });
